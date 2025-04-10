4a1bce6978a17bd6f5e95b4dc060094ea7ca6322
jaswanth <jaswanthchowdari@gmail.com>
2025-04-10 10:54:58 +0530

    teeraform-docker

---
+++ terraform-docker/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_server"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
+
